95ba1286616dfddcc8d2c7ec8b61a341b685951b
AnanSer <ananserbesa@gmail.com>
2024-09-05 10:50:01 +0300

    updated menu-item.js with styling and directory.component.js

--- src/components'/directory-menu/directory.component.jsx
+++ src/components'/directory-menu/directory.component.jsx
@@ -1,16 +1,56 @@
 import React from "react";
-
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.style.css";
 class Directory extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      section: [
+      sections: [
         {
-          title: hats,
-          img,
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+
+          id: 1,
+        },
+
+        {
+          title: "Jackets",
+          imageUrl:
+            "C:/Users/user/Pictures/Screenshots/Screenshot 2024-08-04 010137.png",
+          id: 2,
+        },
+        {
+          title: "Snickers",
+          imageUrl:
+            "C:/Users/user/Pictures/Screenshots/Screenshot 2024-08-04 010137.png",
+          id: 3,
+        },
+        {
+          title: "mens",
+          imageUrl:
+            "C:/Users/user/Pictures/Screenshots/Screenshot 2024-08-04 010137.png",
+          id: 4,
+        },
+        {
+          title: "womens",
+          imageUrl:
+            "C:/Users/user/Pictures/Screenshots/Screenshot 2024-08-04 010137.png",
+          id: 5,
         },
       ],
     };
   }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
 }
+
+export default Directory;
--- src/pages/homePages/homePage.component.jsx
+++ src/pages/homePages/homePage.component.jsx
@@ -2,45 +2,12 @@ import React from "react";
 
 import "./homePage.styles.css";
 
+import Directory from "../../components'/directory-menu/directory.component";
+
 const HomePage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle"> SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle"> SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle"> SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle"> SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle"> SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 };
